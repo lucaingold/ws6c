bcbd6a4ea5ea427dfdae73a81d402b33ba45d085
alinaliburkina <alina.liburkina@students.fhnw.ch>
2017-04-25 08:44:51 +0200

    Button add idea added

---
+++ src/pages/create/idea/idea.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NavController, ViewController, NavParams } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'page-category',
+  templateUrl: 'idea.html'
+})
+export class CreateCategoryPage {
+  categories: FirebaseListObservable<any>;
+  form: any;
+  title: string = '';
+  description: string = '';
+  imageUrl: any;
+
+  constructor(public navCtrl: NavController, public af: AngularFire, private _FB: FormBuilder, public viewCtrl: ViewController) {
+    this.categories = this.af.database.list('/categories');
+    this.form = _FB.group({ 'title': ['', Validators.required], 'description': ['', Validators.required] });
+  }
+
+sendPost() {
+    this.categories.push({
+      title: this.title,
+      description: this.description
+    });
+    this.dismiss();
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Upload');
+  }
+}
