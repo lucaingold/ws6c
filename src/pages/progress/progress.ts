9087fcdc43a4e41c84c7fd9159c005e34b904cf4
Luca Ingold <luca.ingold@gmail.com>
2017-06-11 02:37:15 +0200

    fixed bug, progess

--- src/pages/progress/progress.ts
+++ src/pages/progress/progress.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {Chart} from 'chart.js';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'page-progress',
@@ -8,121 +9,100 @@ import {Chart} from 'chart.js';
 })
 export class ProgressPage {
 
+  categories: FirebaseListObservable<any>;
+  dataSummary: any;
+
   @ViewChild('barCanvas') barCanvas;
   @ViewChild('doughnutCanvas') doughnutCanvas;
   @ViewChild('lineCanvas') lineCanvas;
 
-  barChart: any;
   doughnutChart: any;
-  lineChart: any;
 
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+    this.categories = this.af.database.list('/categories');
 
-  constructor(public navCtrl: NavController) {
+    this.categories.subscribe(categories => {
+      this.dataSummary = categories.map(category => {
 
+        let entries = Object.keys(category.entries).map(function (key) {
+          return category.entries[key];
+        });
+
+        let entry = Object.keys(entries).map(function (key) {
+          return entries[key];
+        });
+
+        let count = 0;
+        entry.forEach(e => {
+          console.log(e.status);
+          if (e.status === false) ++count;
+        });
+
+        return [category.title, count];
+      });
+      this.loadCharts(this.dataSummary);
+    });
   }
 
-  ionViewDidLoad() {
 
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
-
-      type: 'bar',
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-
+  loadCharts(summaryData) {
+    let cat_names = summaryData.map(category => {
+      return category[0];
     });
+    let cat_values = summaryData.map(category => {
+      return category[1];
+    });
+
+    function randomColorGenerator() {
+      return '#' + (Math.random().toString(16) + '0000000').slice(2, 8);
+    }
 
     this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
-
       type: 'doughnut',
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: cat_names,
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          hoverBackgroundColor: [
-            "#FF6384",
-            "#36A2EB",
-            "#FFCE56",
-            "#FF6384",
-            "#36A2EB",
-            "#FFCE56"
-          ]
+          data: cat_values,
+          fillColor: randomColorGenerator(),
+          strokeColor: randomColorGenerator(),
+          highlightFill: randomColorGenerator(),
+          highlightStroke: randomColorGenerator()
         }]
       }
-
     });
 
-    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+    // this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+    //
+    //   type: 'line',
+    //   data: {
+    //     labels: ["January", "February", "March", "April", "May", "June", "July"],
+    //     datasets: [
+    //       {
+    //         label: "My First dataset",
+    //         fill: false,
+    //         lineTension: 0.1,
+    //         backgroundColor: "rgba(75,192,192,0.4)",
+    //         borderColor: "rgba(75,192,192,1)",
+    //         borderCapStyle: 'butt',
+    //         borderDash: [],
+    //         borderDashOffset: 0.0,
+    //         borderJoinStyle: 'miter',
+    //         pointBorderColor: "rgba(75,192,192,1)",
+    //         pointBackgroundColor: "#fff",
+    //         pointBorderWidth: 1,
+    //         pointHoverRadius: 5,
+    //         pointHoverBackgroundColor: "rgba(75,192,192,1)",
+    //         pointHoverBorderColor: "rgba(220,220,220,1)",
+    //         pointHoverBorderWidth: 2,
+    //         pointRadius: 1,
+    //         pointHitRadius: 10,
+    //         data: [65, 59, 80, 81, 56, 55, 40],
+    //         spanGaps: false,
+    //       }
+    //     ]
+    //   }
 
-      type: 'line',
-      data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-          {
-            label: "My First dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
-            spanGaps: false,
-          }
-        ]
-      }
-
-    });
+    // });
 
   }
 }
